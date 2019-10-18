6c4feeb4bfba74364a387162c104ff7d502be7a0
Qiu Yu <unicell@gmail.com>
2019-10-17 23:35:53 -0700

    Add pkg-config to setup.sh

--- setup.sh
+++ setup.sh
@@ -14,6 +14,10 @@ sudo apt-get install -qy libelf-dev libcap-dev flex bison
 sudo apt-get install -qy clang llvm gcc-multilib linux-tools-$(uname -r) linux-headers-$(uname -r)
 sudo apt-get install -qy clang-7 llvm-7
 
+#sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-7 50
+#sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-7 50
+#sudo update-alternatives --install /usr/bin/clang-cpp clang-cpp /usr/bin/clang-cpp-7 50
+
 # for bpftool
 sudo apt-get install -y binutils-dev binutils-multiarch-dev libiberty-dev
 
@@ -25,3 +29,6 @@ sudo apt-get install -y libreadline-dev
 
 # /usr/share/bcc/tools, /usr/share/bcc/examples
 sudo apt-get install -y bcc-tools libbcc-examples linux-headers-$(uname -r)
+
+# for xdp-tutorial
+sudo apt-get install -y pkg-config
