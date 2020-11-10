52b0cc6670cfc72727bfd1c6e763d9ede8f4e3c8
Qiu Yu <unicell@gmail.com>
2020-11-10 11:09:14 -0800

    Update pahole script

--- install-pahole.sh
+++ install-pahole.sh
@@ -3,10 +3,13 @@
 # dwarfris not needed with elfutils (>= 0.173)
 llc -march=bpf -mattr=help |& grep dwarfris
 
+# dwarves 1.15 from Ubuntu Focal
+# while 1.16+ required for DWARF to BTF conversion
 # sudo apt-get install -qy dwarves
+
 sudo apt-get install -qy elfutils cmake libdw-dev
 
-git clone https://git.kernel.org/pub/scm/devel/pahole/pahole.git
+# git clone https://git.kernel.org/pub/scm/devel/pahole/pahole.git
 mkdir -p pahole/build
 cd pahole/build
 cmake -D__LIB=lib ..
@@ -15,4 +18,5 @@ cd -
 
 sudo ldconfig /usr/local/lib
 
+pahole --version
 pahole --help | grep BTF
